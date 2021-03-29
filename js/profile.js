47b2386807021b7e768b8e7d34189f52364f2ba8
75 Business <75business@protonmail.com>
2021-03-29 12:41:23 -0700

    Updates

--- js/profile.js
+++ js/profile.js
@@ -6,7 +6,7 @@ const auth = firebase.auth();
 auth.onAuthStateChanged(user => {
     console.log(user);
     if(user.displayName)
-        displayNameHolder.innerText = user.displayName;
+        displayNameHolder.innerText = user.displayName + " 💸✅";
     if(user.photoURL)
         photoHolder.setAttribute('src', user.photoURL);
 })
