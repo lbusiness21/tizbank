7ba617a9cef92bcbe11155d0e19418532ff07cdb
75 Business <75business@protonmail.com>
2021-05-05 06:14:47 -0700

    Updates

--- bro.js
+++ bro.js
@@ -1,10 +1,14 @@
 function myFunction() {
     const auth = firebase.auth();
     const logoutBtn = document.getElementById('logoutButton');
+    const logoutBtn2 = document.getElementById('logoutBtn');
 
     auth.onAuthStateChanged(user => {
         if(!user){
             window.location.assign('index');
+        }
+        if(user.email) {
+            document.getElementById("demo2").innerHTML = "User logged in: " + " " + user.email;
         }
     })
         logoutBtn.addEventListener('click', e => {
@@ -14,5 +18,14 @@ function myFunction() {
         }).catch(error => {
             console.error(error)
         })
+    });
+
+    logoutBtn2.addEventListener('click', e => {
+    e.preventDefault();
+    auth.signOut().then(() => {
+        window.location.assign('index');
+    }).catch(error => {
+        console.error(error)
+    })
     });
 }
