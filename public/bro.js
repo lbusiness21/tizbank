eed3f78778964e8cae75146f9887dca591d52985
75 Business <75business@protonmail.com>
2021-05-05 09:26:49 -0700

    changes made

--- public/bro.js
+++ public/bro.js
@@ -1,43 +1,10 @@
 function myFunction() {
-    var month = new Array();
-    month[0] = "January";
-    month[1] = "February";
-    month[2] = "March";
-    month[3] = "April";
-    month[4] = "May";
-    month[5] = "June";
-    month[6] = "July";
-    month[7] = "August";
-    month[8] = "September";
-    month[9] = "October";
-    month[10] = "November";
-    month[11] = "December";
-    var d = new Date();
-    var n = month[d.getMonth()];
-    var y = d.getFullYear();
-    var di = d.getDate();
-    var m = d.getDate() -1;
-    var j = d.getDate() ;
-    var k = d.getDate();
-    var l = d.getDate();
-    var p = d.getDate();
-    document.getElementById("demo").innerHTML = n +' '  + m + ' '+ y;
-    document.getElementById("demo3").innerHTML = "Sold Woodforest Bank Log: $34,685 May 4th";
-    document.getElementById("demo4").innerHTML = "Added 13 New Wells Fargo Logs May 3rd";
-    document.getElementById("demo5").innerHTML = "Sold Huntington Bank Log Balance: $23,612";
-    document.getElementById("demo6").innerHTML = "Sold BBVA Bank Log Balance: $20,375";
-    document.getElementById("demo7").innerHTML = "Sold Chime Bank Log Balance: $34,961";
-    document.getElementById("demo8").innerHTML = "Sold Barclays Bank Log Balance: $15,207";
     const auth = firebase.auth();
     const logoutBtn = document.getElementById('logoutButton');
-    const logoutBtn2 = document.getElementById('logoutBtn');
 
     auth.onAuthStateChanged(user => {
         if(!user){
             window.location.assign('index');
-        }
-        if(user.email) {
-            document.getElementById("demo2").innerHTML = "User logged in: " + " " + user.email;
         }
     })
         logoutBtn.addEventListener('click', e => {
@@ -47,14 +14,5 @@ function myFunction() {
         }).catch(error => {
             console.error(error)
         })
-    });
-
-    logoutBtn2.addEventListener('click', e => {
-    e.preventDefault();
-    auth.signOut().then(() => {
-        window.location.assign('index');
-    }).catch(error => {
-        console.error(error)
-    })
     });
 }
