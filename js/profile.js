5f4852469bba59518983e33462842eac601ee9e3
75 Business <75business@protonmail.com>
2021-05-05 09:28:47 -0700

    Updates

--- js/profile.js
+++ js/profile.js
@@ -1,49 +1,20 @@
 const displayNameHolder = document.getElementById('displayNameHolder');
+const jinaHolder = document.getElementById('jinaHolder');
 const photoHolder = document.getElementById('photoHolder');
 
-const jinaHolder = document.getElementById('jinaHolder');
-const imgHolder = document.getElementById('imgHolder')
-
 const emailHolder = document.getElementById('emailHolder');
+const invoiceHolder = document.getElementById('invoiceHolder');
 
 const auth = firebase.auth();
 
-const logoutBtn = document.getElementById('logoutButton');
-const logoutBtn2 = document.getElementById('logoutBtn');
-
 auth.onAuthStateChanged(user => {
     console.log(user);
-    if(user.displayName) {
+    if(user.displayName)
         displayNameHolder.innerText = user.displayName;
         jinaHolder.innerText = user.displayName;
-    }
-    if(user.photoURL) {
+    if(user.photoURL)
         photoHolder.setAttribute('src', user.photoURL);
-        imgHolder.setAttribute('src', user.photoURL);
-    }
-    if(user.email) {
-        emailHolder.innerText = "Invoice to: " + user.email;
-    }
-    if(!user) {
-        window.location.assign('index');
-    }
+    if(user.email)
+        emailHolder.innerText = user.email;
+        invoiceHolder.innerText = "Invoice to: " + " " + user.email ;
 })
-
-
-logoutBtn.addEventListener('click', e => {
-  e.preventDefault();
-  auth.signOut().then(() => {
-      window.location.assign('index');
-  }).catch(error => {
-      console.error(error)
-  })
-});
-
-logoutBtn2.addEventListener('click', e => {
-    e.preventDefault();
-    auth.signOut().then(() => {
-        window.location.assign('index');
-    }).catch(error => {
-        console.error(error)
-    })
-  });
