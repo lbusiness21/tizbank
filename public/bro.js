f31789ca1fb8a86d057bcd57a1e18f39025feda3
75 Business <75business@protonmail.com>
2021-05-05 06:27:22 -0700

    changes made

--- public/bro.js
+++ public/bro.js
@@ -7,25 +7,13 @@ function myFunction() {
         if(!user){
             window.location.assign('index');
         }
-        if(user.email) {
-            document.getElementById("demo2").innerHTML = "User logged in: " + " " + user.email;
-        }
     })
-        logoutBtn.addEventListener('click', e => {
+    logoutBtn.addEventListener('click', e => {
         e.preventDefault();
         auth.signOut().then(() => {
             window.location.assign('index');
         }).catch(error => {
             console.error(error)
         })
-    });
-
-    logoutBtn2.addEventListener('click', e => {
-    e.preventDefault();
-    auth.signOut().then(() => {
-        window.location.assign('index');
-    }).catch(error => {
-        console.error(error)
-    })
     });
 }
