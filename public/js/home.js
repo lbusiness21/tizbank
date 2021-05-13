cd28ccd97203ab4c6941e472f0b1ca0b673b1ff7
75 Business <75business@protonmail.com>
2021-05-13 11:32:00 -0700

    chnage smde

--- public/js/home.js
+++ public/js/home.js
@@ -1,10 +1,4 @@
 const signInWithGoogleButton = document.getElementById('signInWithGoogle');
-
-const phoneNumberField = document.getElementById('phoneNumber');
-const codeField = document.getElementById('code');
-const signInWithPhoneButton = document.getElementById('signInWithPhone');
-
-
 const auth = firebase.auth();
 
 const signInWithGoogle = () => {
@@ -18,32 +12,6 @@ const signInWithGoogle = () => {
   })
 }
 signInWithGoogleButton.addEventListener('click', signInWithGoogle);
-
-const sendVerificationCode = () => {
-  const phoneNumber = phoneNumberField.value;
-
-  auth.signInWithPhoneNumber(phoneNumber)
-  .then(confirmationResult => {
-    const sentCodeId = confirmationResult.verificationId;
-    signInWithPhoneButton.addEventListener('click', () => signInWithPhone(sentCodeId));
-  })
-}
-
-const signInWithPhone = sentCodeId => {
-  const code = codeField.value;
-  const credential = firebase.auth.PhoneAuthProvider.credential(sentCodeId, code);
-  auth.signInWithCredential(credential)
-  .then(() => {
-    window.location.assign('huntington');
-  })
-  .catch(error => {
-    console.error(error);
-  })
-}
-getCodeButton.addEventListener('click', sendVerificationCode);
-
-
-
 
 auth.onAuthStateChanged(user => {
   if(user)
