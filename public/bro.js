8049c6bed49f207baf5c939ae77ff9e010b1c879
75 Business <75business@protonmail.com>
2021-05-16 10:03:16 -0700

    changes made

--- public/bro.js
+++ public/bro.js
@@ -1,13 +1,4 @@
 function myFunction() {
-    var month = new Array();
-    month[4] = "May";
-    month[5] = "June";
-    month[6] = "July";
-    var d = new Date();
-    var n = month[d.getMonth()];
-    var y = d.getFullYear();
-    var m = d.getDate();
-    document.getElementById("demo").innerHTML = n +' '  + m + ' '+ y;
     const auth = firebase.auth();
     const logoutBtn = document.getElementById('logoutButton');
 
