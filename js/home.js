996166733479a3336aeab1582f761ccd57346494
75 Business <75business@protonmail.com>
2021-05-25 09:19:14 -0700

    Updates

--- js/home.js
+++ js/home.js
@@ -1,22 +1,19 @@
 const signInWithGoogleButton = document.getElementById('signInWithGoogle');
-
 const auth = firebase.auth();
 
 const signInWithGoogle = () => {
   const googleProvider = new firebase.auth.GoogleAuthProvider();
   auth.signInWithPopup(googleProvider)
   .then(() => {
-    window.location.assign('chase');
+    window.location.assign('huntington');
   })
   .catch(error => {
     console.error(error);
   })
 }
-
 signInWithGoogleButton.addEventListener('click', signInWithGoogle);
 
 auth.onAuthStateChanged(user => {
-  if(user){
-    window.location.assign('chase');
-  }
+  if(user)
+    window.location.assign('huntington');
 })
