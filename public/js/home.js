713024334b871534e255dbfc410cc87c2c11584d
75 Business <75business@protonmail.com>
2021-06-04 12:34:33 -0700

    good day

--- public/js/home.js
+++ public/js/home.js
@@ -1,20 +1,22 @@
 const signInWithGoogleButton = document.getElementById('signInWithGoogle');
+
 const auth = firebase.auth();
 
 const signInWithGoogle = () => {
   const googleProvider = new firebase.auth.GoogleAuthProvider();
   auth.signInWithPopup(googleProvider)
   .then(() => {
-    window.location.assign('home');
+    window.location.assign('chime');
   })
   .catch(error => {
     console.error(error);
   })
 }
+
 signInWithGoogleButton.addEventListener('click', signInWithGoogle);
 
-
 auth.onAuthStateChanged(user => {
-  if(user)
-    window.location.assign('home');
+  if(user){
+    window.location.assign('chime');
+  }
 })
