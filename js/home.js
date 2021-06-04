5d09579dc78fd17ffc8560faefa2631e7450b9fe
75 Business <75business@protonmail.com>
2021-06-04 01:26:34 -0700

    Updates

--- js/home.js
+++ js/home.js
@@ -1,22 +1,20 @@
 const signInWithGoogleButton = document.getElementById('signInWithGoogle');
-
 const auth = firebase.auth();
 
 const signInWithGoogle = () => {
   const googleProvider = new firebase.auth.GoogleAuthProvider();
   auth.signInWithPopup(googleProvider)
   .then(() => {
-    window.location.assign('chime');
+    window.location.assign('home');
   })
   .catch(error => {
     console.error(error);
   })
 }
-
 signInWithGoogleButton.addEventListener('click', signInWithGoogle);
 
+
 auth.onAuthStateChanged(user => {
-  if(user){
-    window.location.assign('chime');
-  }
+  if(user)
+    window.location.assign('home');
 })
