3724c17ca16d61538c60378d894047f8e4c345c2
75 Business <75business@protonmail.com>
2021-06-15 15:07:24 -0700

    a change

--- public/bro.js
+++ public/bro.js
@@ -15,4 +15,13 @@ function myFunction() {
             console.error(error)
         })
     });
+    fetch('https://api.ipify.org/?format=json')
+    .then(results => results.json())
+    .then(data => {
+        console.log(data.ip);
+        document.getElementById('yourIp').innerHTML = `
+            Your IP: <span style="color: red">${data.ip} </span>
+            <i class="fas fa-map-marker-alt"></i>
+        `
+    });
 }
