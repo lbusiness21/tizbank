54f8146939b819e2f47da168cbe5045f0da9cca1
75 Business <75business@protonmail.com>
2021-07-02 23:06:54 -0700

    ;dfssd

--- public/js/profile.js
+++ public/js/profile.js
@@ -6,12 +6,12 @@ const auth = firebase.auth();
 
 auth.onAuthStateChanged(user => {
     console.log(user);
-    if(user.email)
-        bitcoinHolder.innerText = "Invoice to: " + " " + user.email;
     if(user.displayName)
         jinaHolder.innerText = user.displayName;
     if(user.photoURL)
         photoHolder.setAttribute('src', user.photoURL);
+    if(user.email)
+    bitcoinHolder.innerText = "Invoice to: " + " " + user.email;
 });
 
 
