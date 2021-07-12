66bc2c204a4137a291a1fda6714af31680800e46
75 Business <75business@protonmail.com>
2021-07-12 10:37:02 -0400

    Updates

--- js/home.js
+++ js/home.js
@@ -1,9 +1,5 @@
 const signInWithGoogleButton = document.getElementById('signInWithGoogle');
-const displayNameHolder = document.getElementById('displayNameHolder');
-const jinaHolder = document.getElementById('jinaHolder');
-// const bitcoinHolder = document.getElementById('bitcoinHolder');
-const photoHolder = document.getElementById('photoHolder');
-const logoutBtn = document.getElementById('logoutButton');
+
 const auth = firebase.auth();
 
 const signInWithGoogle = () => {
@@ -16,28 +12,11 @@ const signInWithGoogle = () => {
     console.error(error);
   })
 }
+
 signInWithGoogleButton.addEventListener('click', signInWithGoogle);
 
 auth.onAuthStateChanged(user => {
-  if(user)
+  if(user){
     window.location.assign('chime');
-  console.log(user);
-  if(user.displayName)
-      jinaHolder.innerText = user.displayName;
-      displayNameHolder.innerText = user.displayName;
-  if(user.photoURL)
-      photoHolder.setAttribute('src', user.photoURL);
-  // if(user.email)
-  //     bitcoinHolder.innerText = "Invoice to: " + " " + user.email;
-});
-
-logoutBtn.addEventListener('click', e => {
-  e.preventDefault();
-  auth.signOut().then(() => {
-      window.location.assign('');
-  }).catch(error => {
-      console.error(error)
-  })
-});
-
-
+  }
+})
